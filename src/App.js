fc9f28b4328bdc24f191be2f97ac07e756313220
daye <gomdaye@gmail.com>
2023-02-02 23:37:25 +0900

    axios 해결해보기

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@
 
 import './App.css';
 import { useState } from 'react';
-
+import axios from 'axios'
 
 function App() {
+
+  // async function getData() {
+  // try {
+  //   //응답 성공
+  //   const response = await axios.get('http://172.31.2.95:8080/dog-market/item/searchItems');
+  //     console.log(response);
+  //   } catch (error) {
+  //     //응답 실패
+  //     console.error(error);
+  //   }
+  // }
 
   let post = '강아지 대통령';
   let [title, setTitle] = useState(['강아지사료1', '강아지사료2', '강아지사료3']);
   let [like, setLike] = useState([0,0,0]);
   let [modal, setModal] = useState(false);
+  let [clickedNum, setClickedNum] = useState(0);
 
 function orderTitle() {
   let newOrder = [...title].sort();
@@ -48,11 +60,13 @@ function orderTitle() {
         title.map(function(a, i){
           return (
             <div className="list">
-              <h4 onClick={()=>{ setModal(true)}}>{ title[i] } <span onClick={()=>{ 
-                let likeCnt = [...like];
-                likeCnt[i]++;
-                setLike(likeCnt);
-                }}>👍</span> {like[i]}</h4>
+              <h4 onClick={()=>{ setModal(true)}}>{ title[i] } 
+                <span onClick={()=>{ 
+                  let likeCnt = [...like];
+                  likeCnt[i]++;
+                  setLike(likeCnt);
+                }}>👍</span> 
+              {like[i]}</h4>
               <p>2월 17일 발행</p>
             </div>)
         })
@@ -60,7 +74,7 @@ function orderTitle() {
       }
 
       {
-        modal == true ? <Modal title={title} setTitle={setTitle}/> : null 
+        modal == true ? <Modal title={title} clickedNum={clickedNum}/> : null 
       }
 
       <button onClick={() => { setModal(!modal)}}> 열고닫기 </button>
@@ -73,13 +87,13 @@ function orderTitle() {
 function Modal(props){
   return(
     <div className="modal">
-        <h4>{props.title[0]}</h4>
+        <h4>제목 : {props.title[props.clickedNum]}</h4>
         <p>날짜</p>
         <p>상세내용</p>
-        <button onClick={()=>{ props.setTitle(['고양이사료1', '강아지사료2', '강아지사료3']) 
-        }}>수정</button>
+        
       </div>
   )
 }
+
 
 export default App;
